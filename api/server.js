f2e64369cb9fcdd47cccb72ad7cf1ace3de7c870
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-11-12 21:45:51 -0500

    register and login working

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const usersRouter = require("../users/users-router");
 const server = express();
+require("dotenv").config()
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
--- users/users-router.js
+++ users/users-router.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const bcryptjs = require("bcryptjs");
 const Users = require("./users-model");
+const restrict = require("./users-middleware");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+
+  return jwt.sign(payload, process.env.JWT_SECRET);
+}
 
 router.post("/register", (req, res) => {
   let user = req.body;
@@ -17,6 +28,36 @@ router.post("/register", (req, res) => {
     })
     .catch((error) => {
       res.status(500).json(error);
+    });
+});
+
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  Users.findBy({ username })
+    .then((user) => {
+      if (user && bcryptjs.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ message:"There was an error" + error });
+    });
+});
+
+router.get("/users", restrict, (req, res, next) => {
+  Users.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Unable to get users" });
     });
 });
 
