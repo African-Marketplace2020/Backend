1c4461ab246fc20607e9b6efd96b1d773f33985e
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-11-13 21:56:08 -0500

    edited index and dbConfig

--- index.js
+++ index.js
@@ -4,18 +4,24 @@ const helmet = require("helmet");
 const usersRouter = require("./users/users-router");
 const server = express();
 require("dotenv").config()
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
 server.use("/api", usersRouter );
-server.get("/", (req, res) => {
-  res.send("Server Running, /API/REGISTER to register a new user (expecting a username, password and role) /API/LOGIN to log in");
-});
-
+// server.get("/", (req, res) => {
+//   res.send("Server Running, /API/REGISTER to register a new user (expecting a username, password and role) /API/LOGIN to log in");
+// });
+server.use((err, req, res, next) => {
+	console.log(err)
+	
+	res.status(500).json({
+		message: "Something went wrong",
+	})
+})
 
 
 server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+  console.log(`Server listening on port ${PORT}`);
 });
