74cd5fa34fc24f77a3c33564cf9c6371903e54ad
AustinJHealy <56621614+AustinJHealy@users.noreply.github.com>
2020-11-16 23:03:13 -0500

    edited welcome message

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
 	res.json({
-		message: "Welcome to the African Marketplace API",
+		message: "Welcome to the African Marketplace API-ENDPOINTS: /API/REGISTER- POST-create a new user--/API/LOGIN- POST- log in with username and password--/API/USERS-GET- get a list of all users(must be logged in)",
 	})
 })
 
